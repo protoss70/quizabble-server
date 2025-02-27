3f5ccf49d78a803f83d36ff990fa4348359540c3
protoss70 <tlegendgaming@gmail.com>
2025-02-27 16:13:06 +0100

    fixed entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,10 @@
 if [ "$ENV" = "production" ]; then
+  echo "Using Production Nginx Config"
+  cp /etc/nginx/templates/prod.conf /etc/nginx/conf.d/default.conf
+
   echo "Starting Certbot for Let's Encrypt SSL..."
   certbot certonly --webroot --webroot-path=/var/www/certbot --email gokdenizk.be@gmail.com --agree-tos --no-eff-email --staging -d api.quizabble.com
+  
   nginx -g "daemon off;"
 else
   echo "Using Local SSL (mkcert)..."
