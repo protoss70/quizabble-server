3116dfcc032e71b15b5f48476116983f308d170a
protoss70 <tlegendgaming@gmail.com>
2025-02-27 16:55:16 +0100

    commented out Certbot generation and adjusted Nginx configuration for HTTP only

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,24 +3,20 @@
 echo "Using Production Nginx Config"
 cp /etc/nginx/templates/prod.conf /etc/nginx/conf.d/default.conf
 
-# Ensure Let's Encrypt directory exists
 mkdir -p /var/www/certbot
 
-# Check if the certificate already exists
-if [ ! -f "/etc/letsencrypt/live/api.quizabble.com/fullchain.pem" ]; then
-  echo "Generating SSL certificate with Certbot..."
-  
-  certbot certonly --webroot --webroot-path=/var/www/certbot --email gokdenizk.be@gmail.com --agree-tos --no-eff-email --staging -d api.quizabble.com
-  
-  if [ $? -ne 0 ]; then
-    echo "Certbot failed! Exiting..."
-    exit 1
-  fi
-else
-  echo "SSL certificate already exists. Skipping Certbot generation."
-fi
+# Comment out Certbot generation completely:
+# if [ ! -f "/etc/letsencrypt/live/api.quizabble.com/fullchain.pem" ]; then
+#   certbot certonly --webroot ...
+#   if [ $? -ne 0 ]; then
+#     echo "Certbot failed! Exiting..."
+#     exit 1
+#   fi
+# else
+#   echo "SSL certificate already exists. Skipping Certbot generation."
+# fi
 
-# Start Nginx only after Certbot is successful
+# Just start Nginx now
 echo "Starting Nginx..."
 nginx -g "daemon off;"
 
